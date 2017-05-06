5f6fdf6269cbc43310201d0530072628eb0dd75f
AzariasB <azarias.boutin@gmail.com>
2017-05-06 10:59:15 +0200

    Added waiting list

---
+++ waiting_list.c
@@ -0,0 +1,98 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 azarias.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "waiting_list.h"
+
+struct waiting_list {
+	stamp queue[MAX_PROCESSES];
+	int last_pos;
+};
+
+/**
+ * Inserts the given stamp
+ * at the given position
+ * before inserting, will
+ * shift all the existing
+ * stamps in the array
+ * @param wl the waiting list to use
+ * @param nw_stamp the stamp to insert
+ * @param index the index where to insert the stamp
+ */
+void insert_at(waiting_list *wl, stamp nw_stamp, int index)
+{
+	if (index < 0 || index >= MAX_PROCESSES) {
+		return;
+	}
+
+	//Shift all the existing ones
+	for (int i = index; i < MAX_PROCESSES - 1; i++) {
+		wl->queue[index + 1] = wl->queue[index];
+	}
+	wl->queue[index] = nw_stamp;
+	wl->last_pos++;
+}
+
+/**
+ * Checks wether the stamp s1 is lower than
+ * the stamp s2
+ * @param s1
+ * @param s2
+ * @return 
+ */
+int is_lower_than(stamp s1, stamp s2)
+{
+	return s1.action_number < s2.action_number || (s1.action_number == s2.action_number && s1.proccess_id < s2.proccess_id);
+}
+
+waiting_list* wl_create()
+{
+	waiting_list *wl = malloc(sizeof(waiting_list));
+	wl->last_pos = 0;
+	return wl;
+}
+
+void wl_destroy(waiting_list* wl)
+{
+	free(wl);
+	wl = 0;
+}
+
+void wl_push(waiting_list* wl, stamp nw_stamp)
+{
+	if (wl->last_pos == MAX_PROCESSES) {
+		return; //not possible to insert
+	}
+
+	for (int i = 0; i < wl->last_pos; i++) {
+		stamp s = wl->queue[i];
+		if (is_lower_than(nw_stamp, s)) {
+			insert_at(wl, nw_stamp, i);
+			return;
+		}
+	}
+	//Not inserted : insert at the end
+	wl->queue[wl->last_pos] = nw_stamp;
+	wl->last_pos++;
+
+}
---
+++ waiting_list.h
@@ -0,0 +1,87 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 azarias.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/* 
+ * File:   waiting_list.h
+ * Author: azarias
+ *
+ * Created on 6 mai 2017, 10:32
+ */
+
+
+#ifndef WAITING_LIST_H
+#define WAITING_LIST_H
+
+#include "observer.h"
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+typedef struct waiting_list waiting_list;
+
+/**
+ * Creates a new waiting list
+ * @return the newly craeted waiting list
+ */
+waiting_list *wl_create();
+
+/**
+ * Destroys the given waiting list
+ * @param wl
+ */
+void wl_destroy(waiting_list* wl);
+
+/**
+ * Add a new stamp in the waiting list
+ * insert it in a sorted way
+ * @param nw_stamp
+ */
+void wl_push(waiting_list *wl, stamp nw_stamp);
+
+/**
+ * checks if the given stamp
+ * is first in the given waiting_list
+ * @param wl the waiting list to use
+ * @param st the element to check
+ * @return 1 if the element is first in the waiting list, 0 otherwise
+ */
+int wl_isnext(waiting_list *wl, stamp st);
+
+/**
+ * Removes the first element ofthe list
+ * and shifts all the others
+ */
+void wl_shift();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* WAITING_LIST_H */
+
